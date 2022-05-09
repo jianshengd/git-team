0cca99a5dc02cacdbcd5e6507c99e1ff6af75ff4
Janson <Janson.Dong@newlandpayment.com>
2022-05-09 10:21:06 +0800

    B create

---
+++ app/src/main/java/com/janson/gittest/B.java
@@ -0,0 +1,8 @@
+package com.janson.gittest;
+
+/**
+ * @author Janson
+ * @date 2022/5/9 10:19
+ */
+public class B {
+} 
--- app/src/main/java/com/janson/gittest/MainActivity.java
+++ app/src/main/java/com/janson/gittest/MainActivity.java
@@ -14,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void base(){
         int a = 2;
-        int b = 3;
-        System.out.println(a+b+"");
+        int bb = 4;
+        System.out.println(a+bb+"");
+    }
+
+    public void b(){
+        System.out.println("b");
     }
 
 }
